3bfe5c12239125c04f00f6ed5869e45c55e6b9fb
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-01-10 13:34:17 +0100

    Better approach for 'hovering' table row

--- tests/Nightwatch/Tests/tableScreenshots.js
+++ tests/Nightwatch/Tests/tableScreenshots.js
@@ -26,8 +26,9 @@ module.exports = {
         .resizeWindow(1024, 600)
         .smartURL((langprefix ? '/' + langprefix : '') + '/table')
         .waitTillElementPresent('table.test-table')
-        .click('[name="table[1]"]')
-        .moveToElement('table.test-table tbody tr:nth-child(3) td:first-child', 10, 10)
+        .click('[name="table[3]"]')
+        .moveToElement('table.test-table tbody tr:nth-child(1) td:first-child', 10, 10)
+        .click('table.test-table tbody tr:nth-child(1)')
         .savefullScreenShot('01', langprefix);
     });
   },
