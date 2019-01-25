a2538389d6ea24f0e2b20276d5f842d70fc3dfac
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-01-25 18:46:12 +0100

    Button test

---
+++ tests/Nightwatch/Commands/focusOn.js
@@ -0,0 +1,32 @@
+/**
+ * Triggers focus by js.
+ *
+ * @param {string} cssSelector
+ *   The selector of the input field.
+ * @param {function} callback
+ *   A callback which will be called, when the creating the use is finished.
+ *
+ * @return {object}
+ *   The 'browser' instance.
+ */
+exports.command = function setDateValue(cssSelector, callback) {
+  const _self = this;
+  this
+    .execute(function () {
+        try {
+          // Chrome throws error on this...
+          document.querySelector(arguments[0]).focus(false);
+        }
+        catch (e) {
+          document.querySelector(arguments[0]).focus();
+        }
+      },
+      [cssSelector]
+    );
+  if (typeof callback === 'function') {
+    callback.call(_self);
+  }
+
+  return this;
+
+};
---
+++ tests/Nightwatch/Commands/saveScreenShot.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * Create and save a screenshot.
+ *
+ * @param {string} namePrefix
+ *   Prefix for the file name, optional.
+ * @param {string} nameSuffix
+ *   Suffix for the file name, optional.
+ * @param {string} name
+ *   Test name override.
+ * @param {bool} override
+ *   Whether the destionation can be overridden if exists. True by default.
+ *
+ * @return {object}
+ *   The 'browser' instance.
+ */
+exports.command = function savefullScreenShot(
+  namePrefix = '',
+  nameSuffix = '',
+  name = '',
+  override = (typeof this.globals.fullScreenShotOverride !== 'undefined' ?
+    this.globals.fullScreenShotOverride : true),
+) {
+
+  const _self = this;
+  const platformName = _self.capabilities.platformName || _self.capabilities.platform || 'nan';
+  const browserName = _self.capabilities.browserName || 'nan';
+  let nameComponents = [(name ? name : _self.currentTest.name), platformName, browserName];
+  if (
+    _self.capabilities.mobileEmulationEnabled &&
+    _self.options.desiredCapabilities.chromeOptions &&
+    _self.options.desiredCapabilities.chromeOptions.mobileEmulation &&
+    _self.options.desiredCapabilities.chromeOptions.mobileEmulation.deviceName) {
+    nameComponents.push(_self.options.desiredCapabilities.chromeOptions.mobileEmulation.deviceName.replace(/\s/g, '').replace(/\//g, ''));
+  }
+  if (namePrefix) {
+    nameComponents.unshift(namePrefix);
+  }
+  if (nameSuffix) {
+    nameComponents.push(nameSuffix);
+  }
+  const fileName = nameComponents.join('--').replace(/\s/g, '-');
+  let fileNameWithPath = [(this.options && this.options.screenshotsPath ?
+    this.options.screenshotsPath : 'screenshots'), this.currentTest.module.split(path.sep).pop(), fileName].join(path.sep);
+
+  if (!override && fs.existsSync(fileNameWithPath + '.png')) {
+    let index = 0;
+    while (fs.existsSync(`${fileNameWithPath}_${index}.png`)) {
+      index++;
+    }
+    fileNameWithPath += `_${index}`;
+  }
+
+  const documentElementTag = platformName === 'iOS' ? 'body' : 'html';
+
+  this
+    .saveScreenshot(fileNameWithPath + '.png')
+    .pause(1);
+
+  return this;
+
+};
